e24c4f55e09bc48f7f75a1b0d4c2c138754e0fb4
Foxouille <tokynightfox@gmail.com>
2019-09-28 22:41:46 +0200

    Menu

--- ui/GUI.gd
+++ ui/GUI.gd
@@ -1,10 +1,10 @@
 extends Control
-#onready var Goods = $Goods
+onready var Goods = $Goods
 onready var Health = $Health
 onready var Crew = $Crew
 
-#func _on_Player_goods_changed(new_goods):
-#	Goods.cur_goods = new_goods
+func _on_Player_goods_changed(new_goods):
+	Goods.cur_goods = new_goods
 
 func _on_Player_health_changed(new_health):
 	Health.cur_health = new_health
