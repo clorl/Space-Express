e47095e096b841a14f561b5442b004ff97a667d7
Topy <topy72.mine@gmail.com>
2019-09-29 15:12:46 +0200

    Corrections menu

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,9 +1,5 @@
 extends Control
 onready var Popup = $Popup
-<<<<<<< HEAD
-=======
-
->>>>>>> Score
 signal scene_changed()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,14 +13,9 @@ func _on_Play_pressed():
 	change_scene("res://Game.tscn")
 
 func _on_Credits_pressed():
-<<<<<<< HEAD
 	Popup.popup()
-=======
 	$Popup.popup()
->>>>>>> Score
 	
-
-
 
 func _on_Croix_pressed():
 	$Popup.visible = false
