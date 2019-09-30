989ba74085d1fc644f34a59aad5db0d9357fcd9b
Topy <topy72.mine@gmail.com>
2019-09-30 11:46:51 +0200

    Export

--- menu/logo.gd
+++ menu/logo.gd
@@ -3,7 +3,7 @@ extends ColorRect
 signal scene_changed()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	OS.window_maximized = true
 
 func _on_logo_animation_finished(anim_name):
 	get_tree().change_scene("res://menu/Menu.tscn")
